b41f522d6fc150a0a185b66ed4c30e3efe1899fc
vjpal3 <vrishalipal@yahoo.com>
2020-05-04 16:58:50 -0400

    Configure JPA and initialise data

--- src/main/java/com/mycompany/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/mycompany/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -16,10 +16,13 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired 
+	private ExchangeValueRepository repository;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
-		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(75));
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from,to);  
 		
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		
--- src/main/java/com/mycompany/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/com/mycompany/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,12 +2,29 @@ package com.mycompany.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Exchange_Value")
 public class ExchangeValue {
 	
+	@Id
+	@Column(name="id")
 	private Long id;  
+	
+	@Column(name="currency_from")
 	private String from;  
-	private String to;  
+	
+	@Column(name="currency_to")
+	private String to; 
+	
+	@Column(name="conversion_multiple")  
 	private BigDecimal conversionMultiple;
+	
+	@Column(name="port")  
 	private int port;
 	
 	public ExchangeValue() {
